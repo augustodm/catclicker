83b34678e4af4ef984b73e31930d55fb8d124cb9
Augusto Mendes <augustomendes.mendes9@gmail.com>
2020-04-06 15:18:36 -0300

    contador funcionando

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var gatos = document.getElementsByClassName ("cat");
+var counter = 0;
 for (i = 0; gatos.length; i++ ){
-    var legenda = document.getElementById("legend");
+    gatos[i].addEventListener("click" , function(){
+        counter++;
+        var frase = document.getElementById('legend');
+        frase.innerHTML = "cliques: " + counter;
+    });
+}
+/*var legenda = document.getElementById("legend");
     document.createElement("FIGCAPTION");
     var frase = document.createTextNode("teste " + i);
     legenda.appendChild(frase);
     var img = document.getElementsByClassName("cat");
-    img.appendChild(legenda);
-}
+    img.appendChild(legenda);*/
