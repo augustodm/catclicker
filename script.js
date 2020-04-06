f5e743d4605ee0ba651092a32016d07f3abd5221
Augusto Mendes <augustomendes.mendes9@gmail.com>
2020-04-06 14:58:00 -0300

    tentando adicionar varios contadores

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var counter= 0;
-function contador(){
-    counter++;
-var frase = document.getElementById('frase');
-frase.innerHTML = "Cliques " + counter;
+var gatos = document.getElementsByClassName ("cat");
+for (i = 0; gatos.length; i++ ){
+    var legenda = document.getElementById("legend");
+    document.createElement("FIGCAPTION");
+    var frase = document.createTextNode("teste " + i);
+    legenda.appendChild(frase);
+    var img = document.getElementsByClassName("cat");
+    img.appendChild(legenda);
 }
